a4638baa6c92a82623989f62c15f74fee13a3fd4
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2025-02-14 14:48:53 -0800

    Update 2025-02-14_beta-binomial.py

--- src/2025-02-14_beta-binomial.py
+++ src/2025-02-14_beta-binomial.py
@@ -13,28 +13,32 @@ class BetaPriorParams:
 
 
 p1 = BetaPriorParams()
+p2 = BetaPriorParams(alpha=5, beta=1)
 
+p = p1
 
 if __name__ == '__main__':
     with pm.Model() as model:
         y = [1, 1, 0]
-        theta = pm.Beta('theta', alpha=p1.alpha, beta=p1.beta)
+        theta = pm.Beta('theta', alpha=p.alpha, beta=p.beta)
         y_obs = pm.Binomial('y_obs', n=1, p=theta, observed=y)
 
         # Sampling without data:
         start = time.time()
-        idata = pm.sample(1000, return_inferencedata=True, progressbar=True)
+        idata = pm.sample(
+            1000, return_inferencedata=True, progressbar=True, cores=8
+        )
         elapsed_seconds = time.time() - start
 
-        prior_samples = pm.sample_prior_predictive(500, model)
+        # prior_samples = pm.sample_prior_predictive(500, model)
 
     az.plot_trace(idata)
     plt.show()
 
     az.summary(idata, kind='stats')
 
-    az.plot_ppc(prior_samples.prior['theta'], kind='kde')
-    plt.show()
+    # az.plot_ppc(prior_samples['theta'], kind='kde')
+    # plt.show()
 
 
 
