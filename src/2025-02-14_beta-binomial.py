f5d254440cc468aa145ed0fc3ccdc9798aed881b
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2025-02-18 11:43:30 -0800

    Update 2025-02-14_beta-binomial.py

--- src/2025-02-14_beta-binomial.py
+++ src/2025-02-14_beta-binomial.py
@@ -15,6 +15,7 @@ class BetaPriorParams:
 p1 = BetaPriorParams()
 p2 = BetaPriorParams(alpha=5, beta=1)
 
+# Select which parameters to use:
 p = p1
 
 if __name__ == '__main__':
@@ -23,29 +24,25 @@ if __name__ == '__main__':
         theta = pm.Beta('theta', alpha=p.alpha, beta=p.beta)
         y_obs = pm.Binomial('y_obs', n=1, p=theta, observed=y)
 
-        # Sampling without data:
         start = time.time()
         idata = pm.sample(
             1000, return_inferencedata=True, progressbar=True, cores=8
         )
         elapsed_seconds = time.time() - start
 
-        # prior_samples = pm.sample_prior_predictive(500, model)
+        prior_samples = pm.sample_prior_predictive(500, model)
+        posterior_samples = pm.sample_posterior_predictive(idata, model=model)
+
+    az.summary(idata, kind='stats')
 
     az.plot_trace(idata)
     plt.show()
 
-    az.summary(idata, kind='stats')
+    az.plot_ppc(prior_samples, group='prior')
+    plt.show()
 
-    # az.plot_ppc(prior_samples['theta'], kind='kde')
-    # plt.show()
-
-
-
-    print(elapsed_seconds)
-    print(idata)
-
-
+    az.plot_ppc(posterior_samples, group='posterior')
+    plt.show()
 
     print('done')
 
